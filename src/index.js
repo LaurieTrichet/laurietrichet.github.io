2011fd1a700ee84be0266c506724e2ec7ad0c1f7
Laurie Trichet <laurie@twobulls.com>
2021-07-26 14:19:46 +1000

    add contact me section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 
 
 import "../scss/_custom.scss";
+
+function emailComponent() {
+    const element = document.getElementById('contact-email');
+
+    element.addEventListener("click", function(event){
+        window.location.href = "mailto:laurie.trichet@pm.me";
+    });
+}
+
+emailComponent();
